3a931a8eb1f1756007ebbfe6e64ecc72349ec99b
Eric Young <eric.young@ntnu.no>
2021-01-20 21:21:24 +0100

    second try
    don't know

--- app.py
+++ app.py
@@ -1,30 +1,14 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import geopandas as gpd
 import plotly.express as px
 
 Plants = pd.read_excel('Plants_Plot_Data.xlsx', sheet_name='PlotData')
 Plants.head(2)
 
-import plotly.express as px
-
-beer_fig = px.scatter_geo(Plants, 
-#                      locations="iso_alpha",
-                    lon = Plants['long'],
-                    lat = Plants['lat'],
-                    color="type", # which column to use to set the color of markers
-                    hover_name="name", # column added to hover information
-                    size="capacity", # size of markers
-                    projection="natural earth")
-
-
 ########### Define your variables
-beers=['Changed Stout', 'Snake Dog IPA', 'Imperial Porter', 'Does this work Dog IPA']
+beers=['Chained Stout', 'Snake Dog IPA', 'Imperial Porter', 'Does this work Dog IPA']
 ibu_values=[35, 60, 85, 75]
 abv_values=[5.4, 7.1, 9.2, 4.3]
 color1='darkred'
@@ -34,28 +18,15 @@ tabtitle='beer!'
 myheading='Flying Dog Beers'
 label1='IBU'
 label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
 
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
+beer_fig = px.scatter_geo(Plants, 
+#                      locations="iso_alpha",
+                    lon = Plants['long'],
+                    lat = Plants['lat'],
+                    color="type", # which column to use to set the color of markers
+                    hover_name="name", # column added to hover information
+                    size="capacity", # size of markers
+                    projection="natural earth")
 
 #beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
