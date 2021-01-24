c547fdb23ec648e7059e5a09824ba985aec5209f
Eric Young <eric.young@ntnu.no>
2021-01-24 13:32:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,45 +4,35 @@ import dash_html_components as html
 import pandas as pd
 import plotly.express as px
 
-Plants = pd.read_excel('Plants_Plot_Data.xlsx', sheet_name='PlotData')
-Plants.head(2)
+latitude=[-30,-32,-28]
+longitude=[121,115,122]
+facilities=['Kalgoorlie Smelter','Kwinana Refinery', 'Murrin Murrin']
+process = ['Matte Smelter', 'Metal Refinery','Lat Leach']
+capacity=[110,79,40]
+          
+#d = {'col1': [1, 2], 'col2': [3, 4]}
+#df = pd.DataFrame(data=d)
 
-########### Define your variables
-beers=['Chained Stout', 'Snake Dog IPA', 'Imperial Porter', 'Does this work Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
 
-beer_fig = px.scatter_geo(Plants, 
-#                      locations="iso_alpha",
-                    lon = Plants['long'],
-                    lat = Plants['lat'],
-                    color="type", # which column to use to set the color of markers
-                    hover_name="name", # column added to hover information
-                    size="capacity", # size of markers
+Refine_fig = px.scatter_geo(
+                    lon = longitude,
+                    lat = latitude,
+                    color=process, # which column to use to set the color of markers
+                    hover_name=facilities, # column added to hover information
+                    size=capacity, # size of markers
                     projection="natural earth")
-
-#beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
-
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title=tabtitle
+app.title='NickelRefineries'
 
 ########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(myheading),
+    html.H1('Refine'),
     dcc.Graph(
         id='flyingdog',
-        figure=beer_fig
+        figure=Refine_fig
     )]
 )
 
