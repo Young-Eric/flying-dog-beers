1fe6d747f2cc58f669261e528fb306f8781a114f
Eric Young <eric.young@ntnu.no>
2021-03-04 14:43:12 +0100

    linked to excel sheet

--- app.py
+++ app.py
@@ -4,22 +4,23 @@ import dash_html_components as html
 import pandas as pd
 import plotly.express as px
 
-latitude=[-30,-32,-28]
-longitude=[121,115,122]
-facilities=['Kalgoorlie Smelter','Kwinana Refinery', 'Murrin Murrin']
-process = ['Matte Smelter', 'Metal Refinery','Lat Leach']
-capacity=[110,79,40]
+Plants_df = pd.read_excel('Plants_Plot_Data.xlsx')
+# latitude=[-30,-32,-28]
+# longitude=[121,115,122]
+# facilities=['Kalgoorlie Smelter','Kwinana Refinery', 'Murrin Murrin']
+# process = ['Matte Smelter', 'Metal Refinery','Lat Leach']
+# capacity=[110,79,40]
           
 #d = {'col1': [1, 2], 'col2': [3, 4]}
 #df = pd.DataFrame(data=d)
 
 
-Refine_fig = px.scatter_geo(
-                    lon = longitude,
-                    lat = latitude,
-                    color=process, # which column to use to set the color of markers
-                    hover_name=facilities, # column added to hover information
-                    size=capacity, # size of markers
+Refine_fig = px.scatter_geo(data_frame= Plants_df,
+                    lon = 'long',
+                    lat = 'lat',
+                    color='type', # which column to use to set the color of markers
+                    hover_name='name', # column added to hover information
+                    size='capacity', # size of markers
                     projection="natural earth")
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
